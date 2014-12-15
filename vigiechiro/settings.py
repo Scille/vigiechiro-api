00d85e6ee9855c46ab663f7dfcc82b32b37f4ab0
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2014-12-15 16:48:05 +0100

    Add default settings for test env

--- vigiechiro/settings.py
+++ vigiechiro/settings.py
@@ -1,14 +1,21 @@
 from os import environ
 from authomatic.providers import oauth2
 
-### Eve ###
+
+### App ###
+SECRET_KEY = environ.get('SECRET_KEY', 'secret_for_test_only')
+FRONTEND_DOMAIN = environ.get('FRONTEND_DOMAIN', 'http://www.lvh.me:9000')
+PORT = int(environ.get('BACKEND_PORT', 8080))
+BACKEND_DOMAIN = environ.get('BACKEND_DOMAIN', 'http://api.lvh.me:{}'.format(PORT))
+
 MONGO_HOST = environ.get('MONGO_HOST', 'localhost')
 MONGO_PORT = int(environ.get('MONGO_PORT', 27017))
 MONGO_USERNAME = environ.get('MONGO_USERNAME', '')
 MONGO_PASSWORD = environ.get('MONGO_PASSWORD', '')
-
 MONGO_DBNAME = 'vigiechiro'
-X_DOMAINS=environ['FRONTEND_DOMAIN']
+
+### Eve ###
+X_DOMAINS=FRONTEND_DOMAIN
 X_HEADERS=['Accept', 'Content-type', 'Authorization']
 X_EXPOSE_HEADERS=X_HEADERS
 
@@ -41,8 +48,8 @@ DOMAIN = {
 AUTHOMATIC = {
     'github': {
         'class_': oauth2.GitHub,
-        'consumer_key': environ['GITHUB_API_KEY'],
-        'consumer_secret': environ['GITHUB_API_SECRET'],
+        'consumer_key': environ.get('GITHUB_API_KEY', ''),
+        'consumer_secret': environ.get('GITHUB_API_SECRET', ''),
         'scope': oauth2.GitHub.user_info_scope,
         'access_headers': {'User-Agent': 'Awesome-Octocat-App'},
         '_apis': {
@@ -52,14 +59,8 @@ AUTHOMATIC = {
     },
     'google': {
         'class_': oauth2.Google,
-        'consumer_key': environ['GOOGLE_API_KEY'],
-        'consumer_secret': environ['GOOGLE_API_SECRET'],
+        'consumer_key': environ.get('GOOGLE_API_KEY', ''),
+        'consumer_secret': environ.get('GOOGLE_API_SECRET', ''),
         'scope': oauth2.Google.user_info_scope
     }
 }
-
-### App ###
-SECRET_KEY = environ['SECRET_KEY']
-FRONTEND_DOMAIN = environ['FRONTEND_DOMAIN']
-PORT = int(environ.get('BACKEND_PORT', 8080))
-BACKEND_DOMAIN = environ.get('BACKEND_DOMAIN', 'http://api.lvh.me:{}'.format(PORT))
