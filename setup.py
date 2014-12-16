2a5e1b3b422f875c702431dcc72c707cbb7cd0fc
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2014-12-16 17:55:50 +0100

    Eve dependancy is fetch from github

--- setup.py
+++ setup.py
@@ -2,39 +2,27 @@
 
 from setuptools import setup, find_packages
 
-import vigiechiro
-
 setup(
 
     name='vigiechiro',
-    version=vigiechiro.__version__,
-
-    # Use custom Eve (using dev branch) and Authomatics (custom
-    # python3 port) packages
-    packages=["vigiechiro", "eve", "authomatic"],
+    packages=find_packages(),
     author="Scille SAS",
     author_email="contact@scille.eu",
     description="Projet viginature du Museum national d'histoire naturelle",
     long_description=open('README.md').read(),
+    # Eve 0.5 is not yet available
+    dependency_links=['https://github.com/nicolaiarocci/eve/tarball/master#egg=Eve-0.5'],
     install_requires=[
-        # Eve==0.5 (not available for the moment)
-        # Eve's depandancies
-        "Cerberus==0.7.2",
-        "Events==0.2.1",
-        "Flask-PyMongo==0.3.0",
-        "Flask==0.10.1",
-        "itsdangerous==0.24",
-        "Jinja2==2.7.3",
-        "MarkupSafe==0.23",
-        "pymongo==2.7.1",
-        "simplejson==3.5.3",
-        "Werkzeug==0.9.6",
+        "Eve<=0.5",
         "redis",
         # authomatic (not available for the moment for Python3)
         # authomatic's dependancies
         "six"
     ],
-
+    tests_require=[
+        "pytest",
+        "requests"
+    ],
     # Add non-python files with MANIFEST.in
     # include_package_data=True,
 
