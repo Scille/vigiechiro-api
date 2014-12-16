6c24eaa708a84334db97e264775eaba964eeed29
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2014-12-16 12:01:58 +0100

    Correct requirement dependancies

--- setup.py
+++ setup.py
@@ -16,25 +16,23 @@ setup(
     author_email="contact@scille.eu",
     description="Projet viginature du Museum national d'histoire naturelle",
     long_description=open('README.md').read(),
-    install_requires= [
-    # Eve==0.5 (not available for the moment)
-	# Eve's depandancies
-	"Cerberus==0.7.2",
-	"Events==0.2.1",
-	"Flask-PyMongo==0.3.0",
-	"Flask==0.10.1",
-	"itsdangerous==0.24",
-	"Jinja2==2.7.3",
-	"MarkupSafe==0.23",
-	"pymongo==2.7.1",
-	"simplejson==3.5.3",
-	"Werkzeug==0.9.6",
-
-	"redis",
-
-	# authomatic (not available for the moment for Python3)
-	# authomatic's dependancies
-	"six"
+    install_requires=[
+        # Eve==0.5 (not available for the moment)
+        # Eve's depandancies
+        "Cerberus==0.7.2",
+        "Events==0.2.1",
+        "Flask-PyMongo==0.3.0",
+        "Flask==0.10.1",
+        "itsdangerous==0.24",
+        "Jinja2==2.7.3",
+        "MarkupSafe==0.23",
+        "pymongo==2.7.1",
+        "simplejson==3.5.3",
+        "Werkzeug==0.9.6",
+        "redis",
+        # authomatic (not available for the moment for Python3)
+        # authomatic's dependancies
+        "six"
     ],
 
     # Add non-python files with MANIFEST.in
@@ -43,7 +41,7 @@ setup(
     url='http://github.com/scille/vigiechiro-api',
     classifiers=[
         "Programming Language :: Python",
-		"License :: OSI Approved :: GNU General Public License v2 (GPLv2)"
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)"
         "Natural Language :: French",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
