a4767cb73cb0e77b52722949ecc467bd2dbfb130
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2014-12-20 13:31:06 +0100

    Fix broken indentation

--- vigiechiro/resources/site.py
+++ vigiechiro/resources/site.py
@@ -15,31 +15,51 @@ STOC_SCHEMA = {
 class Site(Resource):
     RESOURCE_NAME = 'sites'
     DOMAIN = {
-        'item_title': 'site', 'resource_methods': [
-            'GET', 'POST'], 'item_methods': [
-            'GET', 'PATCH', 'PUT'], 'schema': {
-                'numero': {
-                    'type': 'integer', 'required': True}, 'protocole': {
-                        'type': 'objectid', 'data_relation': {
-                            'resource': 'protocoles', 'field': '_id', 'embeddable': False}, 'commentaire': {
-                                'type': 'string'}, 'numero_grille_stoc': {
-                                    'type': 'string'}, 'verrouiller': {
-                                        'type': 'boolean'}, 'coordonnee': {
-                                            'type': 'point'}, 'url_cartographie': {
-                                                'type': 'url'}, 'largeur': {
-                                                    'type': 'number'}, 'localite': {
-                                                        'type': 'list', 'schema': {
-                                                            'coordonnee': {
-                                                                'type': 'point'}, 'representatif': {
-                                                                    'type': 'boolean'}, 'habitat': {
-                                                                        'type': 'dict', 'schema': {
-                                                                            'date': {
-                                                                                'type': 'datetime'}, 'stoc_principal': {
-                                                                                    'type': 'dict', 'schema': STOC_SCHEMA}, 'stoc_secondaire': {
-                                                                                        'type': 'dict', 'schema': STOC_SCHEMA}}}}}, 'type_site': {
-                                                                                            'type': 'string', 'regex': r'^(LINEAIRE|POLYGONE)$'}, 'generee_aleatoirement': {
-                                                                                                'type': 'boolean'}, 'justification_non_aleatoire': {
-                                                                                                    'type': 'string'}}}}
+        'item_title': 'site',
+        'resource_methods': ['GET', 'POST'],
+        'item_methods': ['GET', 'PATCH', 'PUT'],
+        'schema': {
+            'numero': {'type': 'integer', 'required': True},
+            'protocole': {
+                'type': 'objectid',
+                'data_relation': {
+                    'resource': 'protocoles',
+                    'field': '_id',
+                    'embeddable': False
+                },
+                'commentaire': {'type': 'string'},
+                'numero_grille_stoc': {'type': 'string'},
+                'verrouiller': {'type': 'boolean'},
+                'coordonnee': {'type': 'point'},
+                'url_cartographie': {'type': 'url'},
+                'largeur': {'type': 'number'},
+                'localite': {
+                    'type': 'list',
+                    'schema': {
+                        'coordonnee': {'type': 'point'},
+                        'representatif': {'type': 'boolean'},
+                        'habitat': {
+                            'type': 'dict',
+                            'schema': {
+                                'date': {'type': 'datetime'},
+                                'stoc_principal': {
+                                    'type': 'dict',
+                                    'schema': STOC_SCHEMA
+                                },
+                                'stoc_secondaire': {
+                                    'type': 'dict',
+                                    'schema': STOC_SCHEMA
+                                }
+                            }
+                        }
+                    }
+                },
+                'type_site': {'type': 'string', 'regex': r'^(LINEAIRE|POLYGONE)$'},
+                'generee_aleatoirement': {'type': 'boolean'},
+                'justification_non_aleatoire': {'type': 'string'}
+            }
+        }
+    }
 
     def __init__(self):
         super().__init__()
