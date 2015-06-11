031a76660cdc71b72a9bb4d4fed8011533a86b8f
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2015-06-11 11:19:01 +0200

    Add tadaridaC_batch_watcher

--- bin/run_task.py
+++ bin/run_task.py
@@ -13,13 +13,14 @@ USAGE = 'usage : {} [d|c|c_batch|c_batch_watcher|bilan] [now|delay] <resource_id
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     if len(sys.argv) == 3 and sys.argv[2] in ['now', 'delay']:
+        _, script, timing = sys.argv
         if timing == 'delay':
-            runner = lambda f: f.delay(fichier_id)
+            runner = lambda f: f.delay()
         else:
-            runner = lambda f: f(fichier_id)
-        if sys.argv[1] == 'c_batch':
+            runner = lambda f: f()
+        if script == 'c_batch':
             raise SystemExit(runner(tadaridaC_batch))
-        elif sys.argv[1] ==  'c_batch_watcher':
+        elif script ==  'c_batch_watcher':
             raise SystemExit(runner(tadaridaC_batch_watcher))
     elif len(sys.argv) == 4 and sys.argv[1] in ['d', 'c', 'bilan'] and sys.argv[2] in ['now', 'delay']:
         _, script, timing, fichier_id = sys.argv
