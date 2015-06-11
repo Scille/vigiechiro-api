6a4ea179b0647ebbd740695532880a47d7ee924e
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2015-06-11 11:18:15 +0200

    Add tadaridaC_batch_watcher

--- bin/run_task.py
+++ bin/run_task.py
@@ -2,20 +2,25 @@
 
 import logging; logging.basicConfig(level=logging.INFO)
 import sys
-from vigiechiro.scripts import tadaridaD, tadaridaC, tadaridaC_batch, participation_generate_bilan
+from vigiechiro.scripts import (
+    tadaridaD, tadaridaC, tadaridaC_batch, tadaridaC_batch_watcher,
+    participation_generate_bilan)
 
 
-USAGE = 'usage : {} [d|c|c_batch|bilan] [now|delay] <resource_id>'.format(sys.argv[0])
+USAGE = 'usage : {} [d|c|c_batch|c_batch_watcher|bilan] [now|delay] <resource_id>'.format(sys.argv[0])
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    if len(sys.argv) == 3 and sys.argv[1] == 'c_batch' and sys.argv[2] in ['now', 'delay']:
-        timing = sys.argv[-1]
+    if len(sys.argv) == 3 and sys.argv[2] in ['now', 'delay']:
         if timing == 'delay':
-            raise SystemExit(tadaridaC_batch.delay())
+            runner = lambda f: f.delay(fichier_id)
         else:
-            raise SystemExit(tadaridaC_batch())
+            runner = lambda f: f(fichier_id)
+        if sys.argv[1] == 'c_batch':
+            raise SystemExit(runner(tadaridaC_batch))
+        elif sys.argv[1] ==  'c_batch_watcher':
+            raise SystemExit(runner(tadaridaC_batch_watcher))
     elif len(sys.argv) == 4 and sys.argv[1] in ['d', 'c', 'bilan'] and sys.argv[2] in ['now', 'delay']:
         _, script, timing, fichier_id = sys.argv
         if timing == 'delay':
@@ -28,5 +33,4 @@ if __name__ == '__main__':
             raise SystemExit(runner(tadaridaD))
         else:
             raise SystemExit(runner(participation_generate_bilan))
-    else:
-        raise SystemExit(USAGE)
+    raise SystemExit(USAGE)
