0f4b296e2bdece882840f4263507c4ea0f172526
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2015-06-24 09:18:27 +0200

    Celery reserve one task at a time

--- vigiechiro/scripts/celery.py
+++ vigiechiro/scripts/celery.py
@@ -6,6 +6,9 @@ celery_app = Celery('tasks', broker=settings.CELERY_BROKER_URL,
                     include=['vigiechiro.scripts.task_tadarida_c',
                              'vigiechiro.scripts.task_tadarida_d',
                              'vigiechiro.scripts.task_participation'])
+# http://celery.readthedocs.org/en/latest/userguide/optimizing.html#reserve-one-task-at-a-time
+celery_app.conf.CELERYD_PREFETCH_MULTIPLIER = 1
+celery_app.conf.CELERY_ACKS_LATE=True
 
 
 if __name__ == '__main__':
