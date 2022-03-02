47ed1c8ebe7e96727a16f6df7af05f591e2a32c5
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2022-03-02 13:15:19 +0100

    Fix in2p3/start_slurm_check_queue.sh log folder creation

--- in2p3/start_slurm_check_queue.sh
+++ in2p3/start_slurm_check_queue.sh
@@ -5,6 +5,7 @@ JOB_NAME="cq-$VIGIECHIRO_ENV_NAME"
 # Move to the log folder before submitting the job to indicate where
 # the stdout/stderr logs should end up
 LOG_FOLDER="$VIGIECHIRO_DIR/$VIGIECHIRO_ENV_NAME-logs/check_queue"
+mkdir -p $LOG_FOLDER
 pushd $LOG_FOLDER
 
 # --export option provides $VIGIECHIRO_DIR to the job process
