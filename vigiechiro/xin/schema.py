8a2979a4971a34e3dcd797b74b7913dd73be9554
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2022-03-02 12:30:16 +0100

    Fix invalid request check with running script

--- vigiechiro/xin/schema.py
+++ vigiechiro/xin/schema.py
@@ -518,7 +518,12 @@ class Validator(GenericValidator):
     def _run_attribute_postonly(self, context):
         """Field can be altered by non-admin during POST only"""
         post_only = context.schema['postonly']
-        if "request_user" in g and g.request_user['role'] == 'Administrateur':
+        try:
+            request_user = g.request_user
+        except AttributeError:
+            # Not in a request, we are most likely in a batch job
+            return
+        if request_user['role'] == 'Administrateur':
             return
         if post_only and request.method != 'POST':
             context.add_error(ERROR_READONLY_FIELD)
@@ -528,7 +533,12 @@ class Validator(GenericValidator):
         write_roles = context.schema['writerights']
         if isinstance(write_roles, str):
             write_roles = [write_roles]
-        if "request_user" in g and g.request_user['role'] not in write_roles:
+        try:
+            request_user = g.request_user
+        except AttributeError:
+            # Not in a request, we are most likely in a batch job
+            return
+        if request_user['role'] not in write_roles:
             context.add_error(ERROR_READONLY_FIELD)
 
     def _run_type_objectid(self, context):
