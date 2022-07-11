51b62cefab50e34750e7093e7280de907268e083
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2022-07-11 10:08:56 +0200

    Improve vigiechiro/xin/mail.py

--- vigiechiro/xin/mail.py
+++ vigiechiro/xin/mail.py
@@ -23,6 +23,23 @@ class Mail:
                 subtype=subtype
             )
 
+        if self.app.config["MAIL_MODE"].lower() == "ssl":
+            self._send_ssl(recipient, msg)
+        else:
+            assert self.app.config["MAIL_MODE"].lower() == "tls"
+            self._send_ssl(recipient, msg)
+
+    def _send_ssl(self, recipient, msg):
+        with smtplib.SMTP(host=self.app.config["MAIL_SERVER"], port=self.app.config["MAIL_PORT"]) as server:
+            server.starttls()
+            server.login(user=self.app.config["MAIL_USERNAME"], password=self.app.config["MAIL_PASSWORD"])
+            server.send_message(
+                from_addr=self.app.config["MAIL_DEFAULT_SENDER"],
+                to_addrs=[recipient],
+                msg=msg,
+            )
+
+    def _send_tls(self, recipient, msg):
         with smtplib.SMTP_SSL(host=self.app.config["MAIL_SERVER"], port=self.app.config["MAIL_PORT"]) as server:
             server.ehlo()
             server.login(user=self.app.config["MAIL_USERNAME"], password=self.app.config["MAIL_PASSWORD"])
